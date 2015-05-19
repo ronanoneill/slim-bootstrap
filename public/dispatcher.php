31986af9cff649f4db11ae71b3371c956ecde9a7
Rónán O'Neill <ronan@adverts.ie>
2015-05-19 15:30:36 +0100

    Tabs -> whitespace

--- public/dispatcher.php
+++ public/dispatcher.php
@@ -12,19 +12,17 @@
 // Include Composer's autoload https://getcomposer.org/doc/01-basic-usage.md#autoloading
 require '../vendor/autoload.php';
 
-// We're using UTF-8 strings until the end of the script
+// Handle UTF-8 encoding
 mb_internal_encoding('UTF-8');
-
-// We'll be outputting UTF-8 to the browser
 mb_http_output('UTF-8');
 
 // Create instance of Slim
 $app = new \Slim\Slim(
-	array(
-		'debug' => true
-		, 'templates.path' => '../src/Templates'
-		, 'view' => new \Slim\Views\Twig()
-	)
+    array(
+        'debug' => true
+        , 'templates.path' => '../src/Templates'
+        , 'view' => new \Slim\Views\Twig()
+    )
 );
 
 // @TODO: Configure Twig options / extensions
@@ -35,11 +33,11 @@ $app = new \Slim\Slim(
 
 // Homepage
 $app->get('/', function() use ($app) {
-	$homepage = new \Controller\Homepage(
-		$app
-		, new \Model\Homepage()
-	);
-	$homepage->index();
+    $homepage = new \Controller\Homepage(
+        $app
+        , new \Model\Homepage()
+    );
+    $homepage->index();
 })->name('homepage');
 
 // Run the application
