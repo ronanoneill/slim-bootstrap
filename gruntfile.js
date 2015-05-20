b45513271b98cc5dda47226f6323812219e1ead6
Rónán O'Neill <ronan@adverts.ie>
2015-05-20 15:42:39 +0100

    Comment the gruntfile, indentation on package.json

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,8 @@
+// Grunt wrapper
 module.exports = function(grunt) {
+    // Require the just in time plugin loader
     require('jit-grunt')(grunt);
+    // Init the config object, define the less & watch options
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         less: {
@@ -9,17 +12,21 @@ module.exports = function(grunt) {
                     yuicompress: true,
                     optimization: 2
                 },
+                // @TODO, ideally we should define the source files dynamically based on location
                 files: {
-                    "public/static/css/stylesheets/style.css": "public/static/css/compile/style.compile.less"
+                    "public/static/css/stylesheets/style.css":
+                        "public/static/css/compile/style.compile.less"
                 }
             }
         },
         watch: {
             styles: {
+                // Location of the less files on which we want to catch updates
                 files: ['public/static/css/**/*.less'],
                 tasks: ['less']
             }
         }
     });
+    // Register the less and watch tasks by default
     grunt.registerTask('default', ['less', 'watch']);
 };
